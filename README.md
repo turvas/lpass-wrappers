c490fe6a4fe3d4c958476544919c4c7c07912876
kalev kuusik <kalev@bytelife.ee>
2020-04-22 16:02:42 +0300

    added hostname extraction from URL field for ssh

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ LastPass CLI wrappers for Linux/Appple OS X
 Files and functions
 --------------------
 * lpass-wrapper.sh  - can be run all lpass commads, ensuring agent is active, if not then authenticate
-* lpssh.sh - SSH with LastPass inegration, assumes SecureNote of Server type
+* lpssh.sh - SSH with LastPass inegration, assumes SecureNote of Server type, or Site (hostname extracted from URL)
--- lpssh.sh
+++ lpssh.sh
@@ -11,6 +11,10 @@ if [ $? -ne 0 ]; then # if not found, then no point to continue
 fi
 PW=$($DIR/lpass-wrapper.sh show -p $ID)
 HN=$($DIR/lpass-wrapper.sh show --field=Hostname $ID)
+if [ -z "$HN" ]; then # if empty, lets tru URL and extract hostname
+        echo Hostname field not found, trying extract hostname from URL
+        HN=$($DIR/lpass-wrapper.sh show --url $ID | awk -F "/" '{print $3}')
+fi
 echo Connecting: $USR@$HN
 shift
 sshpass -p"$PW" ssh $USR@$HN $@
