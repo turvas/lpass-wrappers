352aba63ad34e57d5bbeb5c6cc77fc07a8d9095b
kalev kuusik <kalev@bytelife.ee>
2020-04-22 15:17:43 +0300

    added some errorhandling to ssh wrapper

--- lpssh.sh
+++ lpssh.sh
@@ -5,8 +5,11 @@
 # asumes wrapper is in same dir
 DIR=$(dirname $0)
 ID=$1
-PW=$($DIR/lpass-wrapper.sh show -p $ID)
 USR=$($DIR/lpass-wrapper.sh show -u $ID)
+if [ $? -ne 0 ]; then # if not found, then no point to continue
+        exit 1
+fi
+PW=$($DIR/lpass-wrapper.sh show -p $ID)
 HN=$($DIR/lpass-wrapper.sh show --field=Hostname $ID)
 echo Connecting: $USR@$HN
 shift
